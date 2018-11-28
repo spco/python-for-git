18ec08e5d35ccf95cc1a2d3eaf40ee22656f649c
Sam Cox <sc676@le.ac.uk>
2018-11-28 16:02:48 +0000

    Add fix to handle multiple input files at once.

--- python/first.py
+++ python/first.py
@@ -5,15 +5,16 @@ import sys
 import os
 
 def main():
-	"""Reads in the supplied data file, and returns the first letter of each word."""
-	assert len(sys.argv) == 2, 'Please enter the location of the input data file.'
-	input_filename = sys.argv[1]
-	assert os.path.isfile(input_filename), 'Input data file ' + str(input_filename) + ' does not exist.'
-	
-	with open(input_filename) as in_file:
-		first_letters = ''.join([word[0] for line in in_file.readlines() for word in line.split()])
+	"""Reads in the supplied data file(s), and returns the first letter of each word."""
+	assert len(sys.argv) >= 2, 'Please enter the location of the input data file(s).'
+	for i in range(1,len(sys.argv)):
+		input_filename = sys.argv[i]
+		assert os.path.isfile(input_filename), 'Input data file ' + str(input_filename) + ' does not exist.'
+		
+		with open(input_filename) as in_file:
+			first_letters = ''.join([word[0] for line in in_file.readlines() for word in line.split()])
 
-	print(first_letters)
+		print(first_letters)
 	
 	return 0
 
