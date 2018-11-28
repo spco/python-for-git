6f7d8e7bb631083060486d2ad1d45deb8fcf3292
Sam Cox <sc676@le.ac.uk>
2018-11-28 14:26:52 +0000

    Add first exercise (first letters of words) and 2 data files.

---
+++ python/first.py
@@ -0,0 +1,22 @@
+# -------------------------
+# Copyright (c) Sam Cox 2018
+# --------------------------
+import sys
+import os
+
+def main():
+	"""Reads in the supplied data file, and returns the first letter of each word."""
+	assert len(sys.argv) == 2, 'Please enter the location of the input data file.'
+	input_filename = sys.argv[1]
+	assert os.path.isfile(input_filename), 'Input data file ' + str(input_filename) + ' does not exist.'
+	
+	with open(input_filename) as in_file:
+		first_letters = ''.join([word[0] for line in in_file.readlines() for word in line.split()])
+
+	print(first_letters)
+	
+	return 0
+
+
+if __name__ == '__main__':
+	main()
